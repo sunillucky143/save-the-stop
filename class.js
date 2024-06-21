75dd5cbd34e4cc30a1275066577acaca506b9e62
sunillucky143 <58873633+sunillucky143@users.noreply.github.com>
2024-06-21 04:58:28 +0000

    update

--- class.js
+++ class.js
@@ -1,40 +1,56 @@
-const {MongoClient, ServerApiVersion} = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-class SST{
-    uri = "mongodb+srv://saitejaalasyam:project123@save-the-stop.7kdmkck.mongodb.net/?retryWrites=true&w=majority&appName=save-the-stop";
-    client = new MongoClient(this.uri, {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    });
-    db="Project_db"
+class SST {
+    constructor() {
+        this.uri = "mongodb+srv://saitejaalasyam:project123@save-the-stop.7kdmkck.mongodb.net/?retryWrites=true&w=majority&appName=save-the-stop";
+        this.client = new MongoClient(this.uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+        this.db = "Project_db";
+    }
 
-
-
-    async connect(client=this.client){
+    async connect() {
         try {
-            await client.connect();
-            return client;
-        } finally {
-            await client.close();
+            await this.client.connect();
+            return this.client;
+        } catch (error) {
+            console.error("Connection failed", error);
         }
     }
-    async fetchData(db=this.db,collection,query)
-    {
-            this.client=this.connect();
-            const database = this.client.db(db);
-            const collections = await client.db("Project_db").listCollections().toArray();
+
+    async fetchData(db, collection, query) {
+        let client;
+        try {
+            client = await this.connect();
+            const database = client.db(db || this.db);
             const collec = database.collection(collection);
             const cursor = collec.find(query);
-
-
-
-
+            const result = await cursor.toArray();
+            return result;
+        } catch (error) {
+            console.error("Fetch failed", error);
+        } finally {
+            if (client) {
+                await client.close();
+            }
+        }
     }
-
 }
 
-var myconnection=new SST();
-myconnection.connect();
+export {SST};
+
+/*(async () => {
+    const myConnection = new SST();
+    const collection = "Kiwi";
+    const query = { "route": collection };
+    try {
+        const ans = await myConnection.fetchData(undefined, collection, query);
+        console.log(ans);
+    } catch (error) {
+        console.error("Error fetching data", error);
+    }
+})();*/

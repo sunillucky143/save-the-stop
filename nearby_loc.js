160f3fc61c18b63151bc3100c0db7abcf4a42df2
SaiTejaAlasyam <78671668+Saitejaalasyam@users.noreply.github.com>
2024-06-20 23:48:40 -0700

    added new files

---
+++ nearby_loc.js
@@ -0,0 +1,59 @@
+const {fetch}=require('./fetch.js');
+(async ()=>
+{
+    const res=await fetch();
+
+    loc(res);
+})();
+
+function dis_cal(lat, long, dis){
+    let lat_cal = dis/111000;
+    let long_c = 111000*Math.cos(lat);
+    let long_cal = dis/long_c;
+
+    const parameters = {
+        n_dis_lat : lat+lat_cal.toFixed(17),
+        s_dis_lat : lat-lat_cal.toFixed(17),
+        e_dis_long : long+long_cal.toFixed(17),
+        w_dis_long : long-long_cal.toFixed(17),
+    };
+
+    return parameters;
+}
+function loc(data)
+{
+   console.log(data);
+    // let ll = [];
+    // let count=0;
+    // var myMap=new Map();
+    // for (let i = 0; i < data.length; i++) {
+    //     let stop = data[i].stops;
+    //     let lat = stop.location.lat;
+    //     let long = stop.location.long;
+    //
+    //     // Print lat and long for debugging
+    //     //console.log(`Checking stop_id: ${stop.stop_id} with lat: ${lat} and long: ${long}`);    uncomment this line to print lat and long
+    //     // Calculate distance from the given lat and long
+    //     let usr_lat = 88.21829440477246;
+    //     let usr_long = -1.8199999999999992;
+    //     let dis = dis_cal(usr_lat, usr_long, 500);
+    //
+    //
+    //     if ((lat.toFixed(17) >= dis.s_dis_lat && lat.toFixed(17) <= dis.n_dis_lat) &&
+    //         (long.toFixed(16) >= dis.w_dis_long && long.toFixed(16) <= dis.e_dis_long)) {
+    //         if (myMap.has(stop.stop_name)) {
+    //             let idx = myMap.get(stop.stop_name);
+    //             let temp = ll[idx];
+    //             temp.push([stop.stop_id, stop.stop_name, data[i].route, data[i].direction_id]);
+    //             ll[idx] = temp;
+    //         } else {
+    //             myMap.set(stop.stop_name, count);
+    //             count++;
+    //             ll.push([stop.stop_id, stop.stop_name, data[i].route, data[i].direction_id]);
+    //         }
+    //     }
+    // }
+    // console.log(ll);
+}
+
+

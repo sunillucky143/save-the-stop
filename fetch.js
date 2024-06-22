da91489603263624bf5179a9523c41c4f96ef179
SaiTejaAlasyam <gamingmvp34@gmail.com>
2024-06-21 23:22:37 -0700

    Updated files

--- fetch.js
+++ fetch.js
@@ -3,18 +3,27 @@ const {SST}=require('./class.js');
 console.log(SST);
 var instance=new SST();
 const collections=["Mango","Kiwi","Papaya","Orange"];
-const result=[];
+let allResults=[];
 
-async function fetch()
+async function fetch( query)
 {
     const myConnection=new SST();
     try{
         for(const collec of collections) {
-            const query = {"route": collec};
-            const ans = await myConnection.fetchData(undefined, collec, query);
-            result.push(ans);
+
+            let db=myConnection.db;
+            const client=myConnection.client;
+            const collection = client.db("db").collection(collec);
+            const ans = await collection.aggregate(query).toArray();
+            allResults = allResults.concat(ans);
+            console.log(ans);
         }
-        return result;
+
+        // Sort all combined results and get the top 3
+        allResults.sort((a, b) => a.distance - b.distance);
+        const topResults = allResults.slice(0, 3);
+
+        return topResults;
     }
     catch (error) {
         console.log("Error Fetching Data",error);
